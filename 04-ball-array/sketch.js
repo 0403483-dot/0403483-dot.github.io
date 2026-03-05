81b821c9b88be27856b601d32aea02e778a247d9
0403483-dot <0403483@spsd.sk.ca>
2026-03-05 10:09:11 -0600

    updated ball to change colors and spawn where mouse is and teleport when goes off screen

--- 04-ball-array/sketch.js
+++ 04-ball-array/sketch.js
@@ -4,6 +4,7 @@ let ballArray = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
 }
 
 function draw() {
@@ -13,24 +14,40 @@ function draw() {
     //move
     ball.x += ball.dx;
     ball.y += ball.dy;
-
+    if(ball.x > width){
+      ball.x = 0;
+    }
+    if(ball.x < 0){
+      ball.x = width;
+    }
+    if(ball.y > height){
+      ball.y = 0;
+    }
+    if (ball.y < 0){
+      ball.y = height;
+    }
+    
     //display
+    fill(ball.r, ball.g, ball.b);
     circle(ball.x, ball.y, ball.radius *2);
   }
 }
 
 function mousePressed(){
-  spawnBall();
+  spawnBall(mouseX, mouseY);
 }
 
 
-function spawnBall(){
+function spawnBall(_x, _y){
   let someBall = {
-    x: random(width),
-    y: random(height),
+    x: _x,
+    y: _y,
     dx: random(-5, 5),
     dy: random(-5, 5),
     radius: random(10, 30),
+    r: random(0,255),
+    g: random(0,255),
+    b: random(0,255),
   };
   ballArray.push(someBall);
 }

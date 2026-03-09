0f08c4f546acd6e76f74682a15f19eb9bdc63a00
0403483-dot <0403483@spsd.sk.ca>
2026-03-09 11:02:43 -0600

    started setting up variables and figuring out the array for building the tower

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -6,10 +6,32 @@
 // - describe what you did to take this project "above and beyond"
 
 
+let tower = [
+  [floor(random(0,2)),floor(random(0,2)),floor(random(0,2))],
+  [floor(random(0,2)),floor(random(0,2)),floor(random(0,2))],
+  [floor(random(0,2)),floor(random(0,2)),floor(random(0,2))],
+  [floor(random(0,2)),floor(random(0,2)),floor(random(0,2))],
+  [floor(random(0,2)),floor(random(0,2)),floor(random(0,2))],
+  
+];
+
+let bet = 25;
+let betMax = 500;
+let betMin = 25;
+let currentRow = 0;
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
+}
+
+
+function drawTower(){
+  for (let tile of tower){
+    
+  }
 }

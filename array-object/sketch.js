3fff0873ecdb01c36f520500cbb7c8f6fc4db44b
0403483-dot <0403483@spsd.sk.ca>
2026-03-12 11:02:47 -0600

    worked on setting up buttons and state variables

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -6,28 +6,32 @@
 // - describe what you did to take this project "above and beyond"
 
 let gameState = "stopped";
+let gameModeDifficulty = "easy";
 
 let rows = 8;
-let cols = 4;
+let columns = 4;
 let bet = 25;
 let betMax = 500;
 let betMin = 25;
 let currentRow = 0;
 let money = 1000;
-
 let gameRow; 
 
+let buttonX; 
+let buttonY;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
+  buttonX = windowWidth*0.5;
+  buttonY = windowHeight*0.9;
 }
 
 function draw() {
   background(25);
   drawText(); 
   drawTower();
+  displayButtons();
 }
 
 
@@ -50,14 +54,14 @@ function drawTower(){
   let spacingX = windowWidth/40;
   let spacingY = windowHeight/50;
 
-  let totalW = cols*tileW + (cols -1)* spacingX;
+  let totalW = columns*tileW + (columns -1)* spacingX;
   let totalH = rows*tileH + (rows -1)* spacingY;
 
   let startX =  windowWidth/2 - totalW/2; // centers the tower
   let startY =  windowHeight/2 - totalH/2;
 
   for (let r = 0; r < rows; r ++){
-    for (let c = 0; c < cols; c++){
+    for (let c = 0; c < columns; c++){
       let x = startX + c * (tileW + spacingX);
       let y = startY + r * (tileH + spacingY);
 
@@ -67,19 +71,42 @@ function drawTower(){
   }
 }
 function mouseWheel(event){  
-  if (event.delta < 0 && bet < betMax){ // can't bet over $500
-    bet+=5;
-  }
-  else if(event.delta > 0 && bet > betMin){ // can't bet under $25
-    bet -=5;
+  if (gameState === "stopped"){
+    if (event.delta < 0 && bet < betMax){ // can't bet over $500
+      bet+=5;
+    }
+    else if(event.delta > 0 && bet > betMin){ // can't bet under $25
+      bet -=5;
+    }
+    return false; // so that the screen doesn't scroll when the mouse wheel scrolls.
   }
 
-  return false; // so that the screen doesn't scroll when the mouse wheel scrolls.
 }
 
-// function mouseClicked(){
-//   if (gameState = "playing" && mouseY >)
-// }
+function placeBet(){
+  money -= bet;
+
+}
+
+function displayButtons(){
+  if (gameState === "stopped"){  
+    rectMode(CENTER);
+    fill(255);
+    rect(buttonX, buttonY, 100, 50);
+
+  }
+
+}
+
+
+function mouseClicked(){
+  
+  
+  // if (bet <= money){
+  //   placeBet();
+  // }
+ 
+}
 
 let tower = {
   
